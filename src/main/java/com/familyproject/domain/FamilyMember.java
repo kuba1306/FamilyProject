9a5fc4cd29f84ed5a6c44d2dce5a209e0f34b43f
kuba1306 <jakubpanienski@wp.pl>
2022-06-05 20:07:11 +0200

    next changes in entities

--- src/main/java/com/familyproject/domain/FamilyMember.java
+++ src/main/java/com/familyproject/domain/FamilyMember.java
@@ -1,58 +1,25 @@
 package com.familyproject.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
-
-
-@RequiredArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 @Table(name = "family_members")
+@AllArgsConstructor
+@NoArgsConstructor
 public class FamilyMember {
-
-    private int memberId;
-    private String givenName;
-    private String familyName;
-    private int age;
-    private Family family;
-
-    public FamilyMember(String givenName, String familyName, int age) {
-        this.givenName = givenName;
-        this.familyName = familyName;
-        this.age = age;
-    }
 
     @Id
     @GeneratedValue
     @NotNull
-    @Column(name = "MEMBER_ID", unique = true)
-    public int getMemberId() {
-        return memberId;
-    }
-
-    @Column(name = "GIVEN_NAME")
-    public String getGivenName() {
-        return givenName;
-    }
-
-    @Column(name = "FAMILY_NAME")
-    public String getFamilyName() {
-        return familyName;
-    }
-
-    @Column(name = "AGE")
-    public int getAge() {
-        return age;
-    }
-
+    private int memberId;
+    private String givenName;
+    private String familyName;
+    private int age;
     @ManyToOne
-    @JoinColumn(name = "MEMBERS")
-    public Family getFamily() {
-        return family;
-    }
+    @JoinColumn(name = "FAMILY_ID")
+    private Family family;
 }
